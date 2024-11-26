7c4e1e72286881e629fe8b5110ed00137ae88603
karu44 <karunakar.ylgf45@gmail.com>
2024-11-26 15:24:19 -0500

    first commit

---
+++ src/test/java/in/karun/__springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.karun.__springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
